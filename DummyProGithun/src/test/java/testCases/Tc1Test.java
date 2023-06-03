c34aa3565bc5bcbe07e29960f68dc99e0fb54276
ani-ruddha <aniruddhadas919@gmail.com>
2023-06-03 16:50:47 +0530

    First Commit By ATE1

---
+++ DummyProGithun/src/test/java/testCases/Tc1Test.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1Test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Tc1 Passed",true);
+	}
+
+}
---
+++ DummyProGithun/src/test/java/testCases/Tc2Test.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test {
+	@Test
+	public void m2()
+	{
+		Reporter.log("Tc2 Passed",true);
+	}
+	
+
+}
