26c0c4ebc491388997afd4ee3b04775f750ffadb
ani-ruddha <aniruddhadas919@gmail.com>
2023-06-05 17:12:59 +0530

    Tc4 commit

--- DummyProGithun/src/test/java/testCases/Tc4.java
+++ DummyProGithun/src/test/java/testCases/Tc4.java
@@ -1,5 +1,14 @@
 package testCases;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Tc4 {
+	
+	@Test
+	public void d1()
+	{
+		Reporter.log("Data Updated",true);
+	}
 
 }
